00b46af78532e8bd6dbd7bab6129ca0becf066e5
Lucía Fernández <lucia.fginer@gmail.com>
2025-06-21 11:21:59 +0300

    Create connection_screen.dart
    Basic structure for the connection screen, to be updated

---
+++ code/lib/screens/connection_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart'; // This line imports the Flutter tools needed to build an app, so it works as a great example
+                                        // What it is doing is importing the library material.dart (which contains common widgets like buttons, text, etc.) from
+                                        // Flutter's SDK (Software Development Kit)
+
+// ConnectionScreen class is the root widget of the app's tree, and all other widgets are built from there
+// This means that all the other parts of the app (buttons, text, etc) will be built from this starting point
+// It extends StatelessWidget, which is a type of widget that never changes once it appears on the screen
+// If we wanted the widget to react to the users actions or update automatically (for example, a counter), we would use StatefulWidget
+
+class ConnectionScreen extends StatelessWidget {
+  const ConnectionScreen({super.key}); // 'super' indicates that, if this widget receives a key, it will be passed to the parent class so Flutter knows about it
+
+  @override
+  Widget build(BuildContext context) {  // Method used to build the UI of this widget
+                                        // The parameter context gives access to theme, size, etc
+    // Scaffold creates an structure for the screen, kind of like an skeleton
+    return Scaffold(
+      // Top bar of the app, often used for navigation or titles
+      appBar: AppBar(
+        title: const Text('Connection'), // Title displayed in the app bar
+      ),
+      // Main content area of the screen
+      body: const Center( // 'Center' places everything in the middle of the screen
+        child: Text('Connecting...'), // Main content
+      ),
+    );
+  }
+}
