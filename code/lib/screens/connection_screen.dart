b76c3808d2b134627fcaeb6ad7ea7e62a508eceb
Lucía Fernández <lucia.fginer@gmail.com>
2025-06-21 11:27:21 +0300

    Update connection_screen.dart
    Minor explanation corrected, still needs to be updated

--- code/lib/screens/connection_screen.dart
+++ code/lib/screens/connection_screen.dart
@@ -1,4 +1,4 @@
-// This file is the connection screen that loads when you first open your screen (check this)
+// Screen dedicated to the login of the user and the connection to the Liquid Galaxy
 
 import 'package:flutter/material.dart'; // This line imports the Flutter tools needed to build an app, so it works as a great example
                                         // What it is doing is importing the library material.dart (which contains common widgets like buttons, text, etc.) from
